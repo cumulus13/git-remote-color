4484b4b08d95b43656331e02461444cc1d70c44f
cumulus13 <cumulus13@gmail.com>
2026-08-18 19:28:04 +0700

    version: 1.0.21 ~ 2026-08-18 19:28:02:935552

--- Formula/git-remote-color.rb
+++ Formula/git-remote-color.rb
@@ -1,7 +1,7 @@
 class GitRemoteColor < Formula
   desc "Colorized, GitHub-aware replacement for git remote -v with rich metadata"
   homepage "https://github.com/cumulus13/git-remote-color"
-  version "1.0.19"
+  version "1.0.21"
   license "MIT"
 
   on_macos do
